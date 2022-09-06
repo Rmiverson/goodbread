738f773b81e2e17f34a368608a46f6049fad5f43
rmiverson <rmiverson99@gmail.com>
2022-09-06 11:37:18 -0700

    deleted routes file since routes is now a reserved keyword for react router and is needed for the new version. moved routing to appjs and will probably just stay there from now on

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import React from 'react'
 import './scss/App.css'
 
-import { Provider } from 'react-redux'
+// import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import store from './store/persistStore'
-import Routes from './routes'
+import { Routes, Route, withRouter } from 'react-router-dom'
+
+import Folder from './containers/Folder'
+import Home from './containers/Home'
+import Profile from './containers/Profile'
+import Recipe from './containers/Recipe'
+import Login from './containers/Login'
+import SubFolder from './containers/SubFolder'
 
 const App = () => {
   return (
-    <Provider store={store}>
+    // <Provider store={store}>
       <BrowserRouter>
-        <div className='App'>
-          <Routes />
-        </div>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            
+            
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/folder/:id' element={<Folder />} />
+            <Route path='/subfolder/:id' element={<SubFolder />} />
+            <Route path='/recipe/:id' element={<Recipe />} />
+          </Routes>
       </BrowserRouter>
-    </Provider>
+    // </Provider>
   )
 }
 
