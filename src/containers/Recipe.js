e3980e9a8c63209581715e7e3d331796e3eb5a18
rmiverson <rmiverson99@gmail.com>
2022-09-08 11:39:33 -0700

    filled out get request inside of the component instead of keeping it in actions, validated that the axios react query uses are working, can access api recive data and display it

--- src/containers/Recipe.js
+++ src/containers/Recipe.js
@@ -1,12 +1,68 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { useQuery } from 'react-query'
+import apiClient from '../http-common'
 
-const Recipe = () => {
-    return (
-        <div className='Recipe'>
-            <h2>Recipe</h2>
+const Recipe = (props) => {
+    const [result, setResult] = useState('loading...')
 
-        </div>
+    const { id } = useParams()
+
+    const { isLoading: isLoadingRecipe, refetch: getRecipeById } = useQuery(
+        'query-recipe-by-id',
+        async () => {
+            return await apiClient.get(`/recipes/${id}`)
+        },
+        {
+            enabled: false,
+            retry: 1,
+            onSuccess: (res) => {
+                const result = {
+                    status: res.status + '-' + res.statusText,
+                    headers: res.headers,
+                    data: res.data
+                }
+                setResult(result)
+            },
+            onError: (err) => {
+                setResult(err.response?.data || err)
+            }
+        }
     )
+
+    useEffect(() => {
+        if (isLoadingRecipe) setResult("loading...")
+    }, [isLoadingRecipe])
+
+    useEffect(() => {
+        ferretRecipeById()
+    }, [])
+
+    function ferretRecipeById() {       
+        if (id) {
+            try {
+                getRecipeById()
+            } catch (err) {
+                setResult(err)
+            }
+        }
+    }
+
+    console.log(result)
+
+    if (result === 'loading...') {
+        return (
+            <span>Loading...</span>
+        )
+    } else {
+        return (
+            <div className='Recipe'>
+                <h2>{result.data.title}</h2>
+                <p>{result.data.description}</p>
+            </div>
+        )        
+    }
+
 }
 
 export default Recipe
