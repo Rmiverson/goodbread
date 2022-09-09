52330e784cf335ee8c2a79046d63734fe7d53d66
rmiverson <rmiverson99@gmail.com>
2022-09-09 11:50:44 -0700

    started on login page, figuring out how redux will work with react query and axios

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { redirect } from 'react-router-dom'
+
+// figure out login and signup api calls with axios and react query
+// need to dispatch and keep user data in local state so needs to work with redux
 
 const Login = () => {
     return (
