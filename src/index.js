1979523b8bd141052fda223044530a0dec7e5687
rmiverson <rmiverson99@gmail.com>
2022-09-16 11:10:25 -0700

    added persist gate which is required for redux persist, in the future it should use a universal loading component instead of the span

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom/client'
 import './scss/index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-
 import { QueryClient, QueryClientProvider } from 'react-query'
-
 import { Provider } from 'react-redux'
-import store from './store/store'
+import { store, persistor } from './store/store'
+import { PersistGate } from 'redux-persist/integration/react'
 
 const queryClient = new QueryClient()
 const root = ReactDOM.createRoot(document.getElementById('root'))
@@ -15,9 +14,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
+      <PersistGate loading={<span>Loading...</span>} persistor={persistor}>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>        
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 )
