1ac32409ddb2ce91c0a957caf472636a853213e4
rmiverson <rmiverson99@gmail.com>
2022-09-16 11:09:25 -0700

    removed a bunch of comment from where i was trying to figure out how to persist my login

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import React, { useEffect } from 'react'
 import './scss/App.css'
 
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
-
 import Folder from './containers/Folder'
 import Home from './containers/Home'
 import Profile from './containers/Profile'
@@ -10,25 +8,8 @@ import Recipe from './containers/Recipe'
 import LoginSignup from './containers/LoginSignup'
 import SubFolder from './containers/SubFolder'
 import ProtectedRoute from './containers/ProtectedRoute'
-import { useDispatch, useSelector } from 'react-redux'
-import { logoutUser, userPersist } from './store/actions/userActions'
 
 const App = () => {
-  // const currentUser = useSelector((state) => state)
-  // const dispatch = useDispatch
-
-  // useEffect(() => {
-    // console.log(currentUser)
-    // const loggedInUser = currentUser
-    // console.log(loggedInUser)
-  // }, [])
-
-  // const handleLogout = (e) => {
-  //   e.preventDefault()
-  //   localStorage.removeItem('token')
-  //   localStorage.removeItem('token_exp')
-  //   dispatch(logoutUser)
-  // }
 
   return (
     <BrowserRouter>
