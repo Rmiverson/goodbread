1bf772093b5e8eb710c0b40f01eaa5e8f71c2875
rmiverson <rmiverson99@gmail.com>
2022-09-19 10:44:27 -0700

    separated routes to their own component

---
+++ src/RouteBoard.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Folder from './containers/Folder'
+import Home from './containers/Home'
+import Profile from './containers/Profile'
+import Recipe from './containers/Recipe'
+import LoginSignup from './containers/LoginSignup'
+import SubFolder from './containers/SubFolder'
+import ProtectedRoute from './containers/ProtectedRoute'
+
+
+
+const RouteBoard = () => {
+    return(
+        <Routes>
+            <Route path='/login-signup' element={<LoginSignup />} />
+            <Route index element={
+                <ProtectedRoute>
+                    <Home />
+                </ProtectedRoute>
+            }/>
+            <Route path='/profile' element={
+                <ProtectedRoute>
+                    <Profile />
+                </ProtectedRoute>
+            }/>
+            <Route path='/folder/:id' element={
+                <ProtectedRoute>
+                    <Folder />
+                </ProtectedRoute>
+            }/>
+            <Route path='/subfolder/:id' element={
+                <ProtectedRoute>
+                    <SubFolder />
+                </ProtectedRoute>
+            }/>
+            <Route path='/recipe/:id' element={
+                <ProtectedRoute>
+                    <Recipe />
+                </ProtectedRoute>
+            }/>                
+            
+            <Route path='*' element={<div>404 not found</div>} />
+        </Routes>
+    )
+}
+
+export default RouteBoard
