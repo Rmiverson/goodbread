bbaf9191864a29b4c61f00b0ffbc7310e13a7be6
rmiverson <rmiverson99@gmail.com>
2022-09-19 10:45:00 -0700

    moved routes to their own component

--- src/App.js
+++ src/App.js
@@ -1,48 +1,12 @@
 import './scss/App.css'
-import { Routes, Route } from 'react-router-dom'
-import Folder from './containers/Folder'
-import Home from './containers/Home'
-import Profile from './containers/Profile'
-import Recipe from './containers/Recipe'
-import LoginSignup from './containers/LoginSignup'
-import SubFolder from './containers/SubFolder'
-import ProtectedRoute from './containers/ProtectedRoute'
 import Nav from './containers/Nav'
+import RouteBoard from './RouteBoard'
 
 const App = () => {  
   return (
     <div className='App'>
       <Nav />
-        <Routes>
-          <Route index 
-          element={
-            <ProtectedRoute>
-              <Home />
-            </ProtectedRoute>
-          }/>
-          <Route path='/login-signup' element={<LoginSignup />} />
-          <Route path='/profile' element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }/>
-          <Route path='/folder/:id' element={
-            <ProtectedRoute>
-              <Folder />
-            </ProtectedRoute>
-          }/>
-          <Route path='/subfolder/:id' element={
-            <ProtectedRoute>
-              <SubFolder />
-            </ProtectedRoute>
-          }/>
-          <Route path='/recipe/:id' element={
-            <ProtectedRoute>
-              <Recipe />
-            </ProtectedRoute>
-          }/>
-          <Route path='*' element={<div>404 not found</div>} />
-        </Routes>
+      <RouteBoard />
     </div>
   )
 }
