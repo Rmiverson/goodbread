3a799e0095aff569a12356aacfd8fb51b615cd11
rmiverson <rmiverson99@gmail.com>
2022-09-19 11:40:56 -0700

    removed a console log that is uneeded, added a link to the new create recipe form

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
     const currentUser = useSelector((state) => state.user)
     return(
         <div className='Home'>
             <h2>Home</h2>
-            {console.log(currentUser)}
+            <Link to='/create-recipe'>Create Recipe</Link>
         </div>
     )
 }
