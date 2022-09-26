a1f2b65c90c10f598971fad24d347b9f00ec33fa
rmiverson <rmiverson99@gmail.com>
2022-09-26 10:56:10 -0700

    updated packag lock, made zindex to listItemIndex to be more informative.

--- src/components/OlForm.js
+++ src/components/OlForm.js
@@ -5,24 +5,24 @@ const OlForm = (props) => {
         <div className='ol-form'>
             <label>Numbered List Title</label>
             <input 
-                    type='text'
-                    placeholder='List Title'
-                    value={props.component.title}
-                    onChange={props.handleComponentTitleChange(props.index)}
+                type='text'
+                placeholder='List Title'
+                value={props.component.title}
+                onChange={props.handleComponentTitleChange(props.index)}
             />
 
             <button type='button' onClick={props.addListItem(props.index)}>Add List Item</button>
             <ol>
-                {props.component.list_items.map((list_item, zIndex) => (
-                    <li key={zIndex}>
+                {props.component.list_items.map((list_item, listItemIndex) => (
+                    <li key={listItemIndex}>
                         <input
                             type='text'
                             placeholder='Item Text'
                             value={list_item}
-                            onChange={props.handleListItemChange(props.index, zIndex)}
+                            onChange={props.handleListItemChange(props.index, listItemIndex)}
                         />
 
-                        <button type='button' onClick={props.removeListItem(props.index, zIndex)}>-</button>
+                        <button type='button' onClick={props.removeListItem(props.index, listItemIndex)}>-</button>
                     </li>
                 ))}
             </ol>
--- src/components/UlForm.js
+++ src/components/UlForm.js
@@ -13,16 +13,16 @@ const UlForm = (props) => {
 
             <button type='button' onClick={props.addListItem(props.index)}>Add List Item</button>
             <ul>
-                {props.component.list_items.map((list_item, zIndex) => (
-                    <li key={zIndex}> 
+                {props.component.list_items.map((list_item, listItemIndex) => (
+                    <li key={listItemIndex}> 
                         <input 
                             type='text'
                             placeholder='Item Text'
                             value={list_item}
-                            onChange={props.handleListItemChange(props.index, zIndex)}
+                            onChange={props.handleListItemChange(props.index, listItemIndex)}
                         />
 
-                        <button type='button' onClick={props.removeListItem(props.index, zIndex)}>-</button>
+                        <button type='button' onClick={props.removeListItem(props.index, listItemIndex)}>-</button>
                     </li>
                 ))}                                        
             </ul>
