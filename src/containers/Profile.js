1917546c270b7aef570a3832d1bb999835482a9b
rmiverson <rmiverson99@gmail.com>
2022-09-28 12:56:27 -0700

    added a consolelog to show user state

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Profile = () => {
+    const currentUser = useSelector((state) => state.user)
+
     return (
         <div className='Profile'>
             <h2>Profile</h2>
+            {console.log(currentUser)}
         </div>
     )
 }
