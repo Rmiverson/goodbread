b1f5dc88f3a79b9c49f3c5d26d5887d8d5fed3a6
rmiverson <rmiverson99@gmail.com>
2022-09-28 12:55:31 -0700

    began major rework with moving recipe rendering to separate component with pagination

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,14 +1,15 @@
 import React, { useEffect } from 'react'
-import { useQuery } from 'react-query'
-import { useSelector, useDispatch } from 'react-redux'
+// import { useQuery } from 'react-query'
+import { useSelector } from 'react-redux' //useDispatch
 import { Link } from 'react-router-dom'
 
-import apiClient from '../http-common'
-import { userUpdate, userLogout } from '../redux/actions'
+import Recipes from './Recipes'
+// import apiClient from '../http-common'
+// import { userUpdate, userLogout } from '../redux/actions'
 
 const Home = () => {
     const currentUser = useSelector((state) => state.user)
-    const dispatch = useDispatch()
+    // const dispatch = useDispatch()
 
     // TODO: make a call fetching all recipes w/ pagination
     // TODO: make a call for folders w/ pagination
@@ -22,56 +23,54 @@ const Home = () => {
     // TODO: strait out error handling and make it more useful for the user
     // TODO: get started on styling
     
-    const { isLoading: isLoadingUser, refetch: getUser } = useQuery(
-        'query-user-by-id',
-        async () => {
-            return await apiClient.get(`/users/${currentUser.id}`, {headers: {'Authorization': `Bearer ${currentUser.token.token}`}})
-        },
-        {
-            enabled: false,
-            retry: 1,
-            onSuccess: (res) => {
-                const result = {
-                    status: res.status + '-' + res.statusText,
-                    headers: res.headers,
-                    data: res.data                    
-                }
+    // const { isLoading: isLoadingUser, refetch: getUser } = useQuery(
+    //     'query-user-by-id',
+    //     async () => {
+    //         return await apiClient.get(`/users/${currentUser.id}`, {headers: {'Authorization': `Bearer ${currentUser.token.token}`}})
+    //     },
+    //     {
+    //         enabled: false,
+    //         retry: 1,
+    //         onSuccess: (res) => {
+    //             const result = {
+    //                 status: res.status + '-' + res.statusText,
+    //                 headers: res.headers,
+    //                 data: res.data                    
+    //             }
 
-                dispatch(userUpdate(result.data))
-            },
-            onError: (err) => {
-                console.error(err.response?.data || err)
-                dispatch(userLogout())
-            }
-        }
-    )
+    //             dispatch(userUpdate(result.data))
+    //         },
+    //         onError: (err) => {
+    //             console.error(err.response?.data || err)
+    //             dispatch(userLogout())
+    //         }
+    //     }
+    // )
 
-    useEffect(() => {
-        function ferretUserById() {
-            try {
-                getUser()
-            } catch (err) {
-                console.error(err)
-                dispatch(userLogout())
-            }
-        }
+    // useEffect(() => {
+    //     function ferretUserById() {
+    //         try {
+    //             getUser()
+    //         } catch (err) {
+    //             console.error(err)
+    //             dispatch(userLogout())
+    //         }
+    //     }
 
-        ferretUserById()
-    }, [getUser, dispatch])
+    //     ferretUserById()
+    // }, [getUser, dispatch])
 
-    if (isLoadingUser) {
-        return <span>Loading...</span>
-    } else {
-        return(
-            <div className='Home'>
-                <h2>Home</h2>
-                {currentUser.recipes.map((recipe, index) => (
-                    <Link key={index} to={`/recipe/${recipe.id}`}>{recipe.title}</Link>
-                ))}
-                <Link to='/create-recipe'>Create Recipe</Link>
-            </div>
-        )        
-    }
+
+    return(
+        <div className='Home'>
+            <h2>Home</h2>
+            {/* {currentUser.recipes.map((recipe, index) => (
+                <Link key={index} to={`/recipe/${recipe.id}`}>{recipe.title}</Link>
+            ))} */}
+            <Link to='/create-recipe'>Create Recipe</Link>
+            <Recipes currentUser={currentUser} />
+        </div>
+    )        
 }
 
 export default Home
