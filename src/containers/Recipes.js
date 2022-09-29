2f706c53821753f7eb9dde6c2949c9c31532220e
rmiverson <rmiverson99@gmail.com>
2022-09-29 16:22:59 -0700

    updated Recipes with better error handling and changed some formatting

--- src/containers/Recipes.js
+++ src/containers/Recipes.js
@@ -6,12 +6,11 @@ import apiClient from '../http-common'
 
 const Recipes = (props) => {
     const currentUser = props.currentUser
-
-    const [currentItems, setCurrentItems] = useState([])
+    const [result, setResult] = useState({data: [], status: null, message: null})
     const [pageCount, setPageCount] = useState(0)
     const [currentPage, setCurrentPage] = useState(0)
 
-    const { isLoading: isLoadingRecipes, refetch: getAllRecipes } = useQuery(
+    const { isLoading: isLoadingRecipes, refetch: getAllUserRecipes } = useQuery(
         'query-all-user-recipes',
         async () => {
             return await apiClient.get(`/recipes/?page=${currentPage + 1}&user_id=${currentUser.id}`, {headers: {'Authorization': `Bearer ${currentUser.token.token}`}})
@@ -20,14 +19,18 @@ const Recipes = (props) => {
             enabled: false,
             retry: 1,
             onSuccess: (res) => {
-                const result = {
+                const apiResp = {
                     status: res.status + '-' + res.statusText,
                     headers: res.headers,
                     data: res.data.data,
                     meta: res.data.meta
                 }
-                setCurrentItems(result.data)
-                setPageCount(result.meta.total_pages)
+                setResult({data: apiResp.data, status: apiResp.status, message: res.statusText})
+                setPageCount(apiResp.meta.total_pages)
+            },
+            onError: (err) => {
+                console.error(err.response?.data || err)
+                setResult({data: [], status: 'Error', message: err.response?.data || err})
             }
         }
     )
@@ -35,14 +38,15 @@ const Recipes = (props) => {
     useEffect(() => {
         function ferretRecipes() {
             try {
-                getAllRecipes()
+                getAllUserRecipes()
             } catch (err) {
                 console.error(err)
+                setResult({data: [], status: 'Error', message: err})  
             }
         }
 
         ferretRecipes()
-    }, [getAllRecipes, currentPage])
+    }, [getAllUserRecipes, currentPage])
 
     const handlePageClick = (e) => {
         setCurrentPage(e.selected)
@@ -50,12 +54,14 @@ const Recipes = (props) => {
 
     if (isLoadingRecipes) {
         return <span>Loading...</span>
+    } else if (result.status === 'Error') {
+        return <span>{result.status + ': ' + result.message}</span>
     } else {
         return (
             <div className='all-recipes'>
                 <h2>All Recipes</h2>
                 <div className='all-recipes-list'>
-                    <RecipeCards items={currentItems} formList={props.formList} />
+                    <RecipeCards items={result.data} formList={props.formList} />
                     <ReactPaginate 
                         breakLabel='...'
                         nextLabel='next >'
