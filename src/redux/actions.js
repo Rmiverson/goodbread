e7f87815d351389193d2e77631ee84b0ff6678bf
rmiverson <rmiverson99@gmail.com>
2022-10-03 10:17:44 -0700

    removed old login and signup actions and reducers from redux, theyre unneeded since we are sending the requests on the component and updating user state through the update user dispatch

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,48 +1,8 @@
 import apiClient from '../http-common'
 
-export function userSignup(user) {   
-   return async dispatch => {
-      try {
-         return await apiClient
-            .post('/signup', JSON.stringify({ user: user }))
-            .then((response) => {
-               let data = response.data
-               dispatch(signupUser(data))
-            })
-      } catch (err) {
-         console.error(err)
-      }
-   }
-}
-
-export function userLogin(user) {
-   return async dispatch => {
-      try {
-         return await apiClient
-            .post('/login', JSON.stringify({ user: user }))
-            .then(( response ) => {
-               let data = response.data
-               dispatch(loginUser(data))
-            })
-      } catch (err) {
-         console.error(err)
-      }
-   }
-}
-
 export function userUpdate(user) {return async dispatch => dispatch(updateUser(user))}
 
 export function userLogout(dispatch) {return dispatch(logoutUser())}
-
- export const signupUser = (userObj) => ({
-   type: 'SIGNUP_USER',
-   payload: userObj
- })
-
- export const loginUser = (userObj) => ({
-    type: 'LOGIN_USER',
-    payload: userObj
- })
  
  export const logoutUser = () => ({
     type: 'LOGOUT_USER'
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -12,10 +12,6 @@ export const initialState = {
 
 export const appReducer = ( state = initialState, action) => {
     switch (action.type) {
-        case 'SIGNUP_USER':
-            return {...state, user: action.payload}
-        case 'LOGIN_USER':
-            return {...state, user: action.payload}
         case 'LOGOUT_USER':
             storage.removeItem('persist:root')
             return {...state, user: {}}
