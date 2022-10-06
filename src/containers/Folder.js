db0b309c12dd4abbfd31b2eca666dbfd29de84b0
rmiverson <rmiverson99@gmail.com>
2022-10-06 11:16:38 -0700

    added a link to edit the folder

--- src/containers/Folder.js
+++ src/containers/Folder.js
@@ -61,6 +61,7 @@ const Folder = () => {
                 <p>{result.data.description}</p>
                 <Link to={`/folder/${id}/subfolder/create`} >Create Sub-Folder</Link>
                 <Link to='/recipe/create'>Create Recipe</Link>
+                <Link to={`/folder/edit/${id}`}>Edit Folder</Link>
                 <SubFolders folderId={id} currentUser={currentUser} />
                 <FolderRecipes folderId={id} currentUser={currentUser} />
             </div>
