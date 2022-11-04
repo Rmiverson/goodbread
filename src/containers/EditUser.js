71a97f40f78cb0f0a00c002d45ff00cdbb4ba297
rmiverson <rmiverson99@gmail.com>
2022-11-04 15:30:12 -0700

    added user delete with confirmation

--- src/containers/EditUser.js
+++ src/containers/EditUser.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
 import { useMutation } from 'react-query'
 import { Navigate } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { userLogout } from '../redux/actions'
 import apiClient from '../http-common'
 
 const EditUser = () => {
@@ -10,6 +11,7 @@ const EditUser = () => {
     const [first_name, setFirst_name] = useState(currentUser.first_name)
     const [last_name, setLast_name] = useState(currentUser.last_name)
     const [description, setDescription] = useState(currentUser.description)
+    const dispatch = useDispatch()
 
     let dataPackage = {}
 
@@ -32,6 +34,34 @@ const EditUser = () => {
             }
         }
     )
+
+    const { isLoading: isDeletingUser, mutate: deleteUser } = useMutation(
+        async () => {
+            return await apiClient.delete(`/users/${currentUser.id}`, {headers: {'Authorization': `Bearer ${currentUser.token.token}`}})
+        },
+        {
+            onSuccess: (res) => {
+                const apiResp = {
+                    status: res.status + '-' + res.statusText,
+                    headers: res.headers,
+                    data: res.data                    
+                }
+                setResult({data: apiResp.data, status: apiResp.status, message: null, submitted: true})
+            },
+            onError: (err) => {
+                console.error(err.response?.data || err)
+                setResult({data: {}, status: 'Error', message: err.response?.data || err, submitted: false})
+            }
+        }
+    )
+    
+    const handleDelete = () => {
+        let warning = "Are you sure you want to delete?\nThis is irreversable and will delete all recipes, folders, and user data.\nClick OK to delete or Cancel."
+        if (window.confirm(warning) === true) {
+            dispatch(userLogout)
+            deleteUser()
+        }
+    }
 
     const handleFirstNameChange = (e) => setFirst_name(e.target.value)
 
@@ -62,10 +92,12 @@ const EditUser = () => {
         }
     }
 
-    if (isPuttingUser) {
+    if (isPuttingUser || isDeletingUser) {
         return <span>Loading...</span>
     } else if (result.submitted) {
         return <Navigate to='/profile' replace />
+    } else if (result.deleted) {
+        return <Navigate to='/login-signup' replace/>
     } else {
         return (
             <div className='edit-user-page'>
@@ -80,6 +112,8 @@ const EditUser = () => {
 
                     <label>Description</label>
                     <textarea name='description' value={description} onChange={handleDescriptionChange} />
+
+                    <input type='button' onClick={handleDelete} value='Delete User Profile' />
 
                     <input type='submit' value='Submit Profile' />
                 </form>
