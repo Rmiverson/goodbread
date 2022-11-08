9b9f1013ff34c20c625c82279144e67c7b134229
rmiverson <rmiverson99@gmail.com>
2022-11-08 11:46:03 -0800

    moved folder and recipe create links from home to their respective components and removed sidebar component since it will be styled within the home component.

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,21 +1,15 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
-import Folders from './Folders'
 import Recipes from './Recipes'
+import Folders from './Folders'
 
 const Home = () => {
     const currentUser = useSelector((state) => state.user)
 
-    // TODO: add deletes for recipes, folder, and users
-    // TODO: get started on styling
-
     return(
-        <div className='Home'>
+        <div className='home'>
             <h2>Home</h2>
-            <Link to='/recipe/create'>Create Recipe</Link>
-            <Link to='/folder/create'>Create Folder</Link>
-            <Recipes currentUser={currentUser} />
+            <Recipes currentUser={currentUser} />                
             <Folders currentUser={currentUser} />
         </div>
     )        
