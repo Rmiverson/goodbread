9495b63bb633114a615f0460fb337a3a3b62224e
rmiverson <rmiverson99@gmail.com>
2022-11-28 12:00:56 -0800

    added a grab icon to be more apparent to the user

--- src/components/OlForm.js
+++ src/components/OlForm.js
@@ -1,35 +1,39 @@
 import React from 'react'
+import {HiMenu} from "react-icons/hi"
 
 const OlForm = (props) => {
     return(
         <>
-            <label>Numbered List Title</label>
-            <input 
-                type='text'
-                placeholder='List Title'
-                value={props.component.title}
-                onChange={props.handleComponentTitleChange(props.index)}
-            />
+            <div className='grab-icon'><HiMenu /></div>
+            <div className='content-input-form'>
+                <label>Numbered List Title</label>
+                <input 
+                    type='text'
+                    placeholder='List Title'
+                    value={props.component.title}
+                    onChange={props.handleComponentTitleChange(props.index)}
+                />
 
-            <button type='button' onClick={props.addListItem(props.index)}>Add List Item</button>
-            <ol>
-                {props.component.list_items.map((list_item, listItemIndex) => (
-                    <li key={listItemIndex}>
-                        <button type='button' onClick={props.handleListItemReorder(props.index, listItemIndex, -1)}>^</button>
-                        <button type='button' onClick={props.handleListItemReorder(props.index, listItemIndex, 1)}>v</button>
-                        <input
-                            type='text'
-                            placeholder='Item Text'
-                            value={list_item}
-                            onChange={props.handleListItemChange(props.index, listItemIndex)}
-                        />
+                <button type='button' onClick={props.addListItem(props.index)}>Add List Item</button>
+                <ol>
+                    {props.component.list_items.map((list_item, listItemIndex) => (
+                        <li key={listItemIndex}>
+                            <button type='button' onClick={props.handleListItemReorder(props.index, listItemIndex, -1)}>^</button>
+                            <button type='button' onClick={props.handleListItemReorder(props.index, listItemIndex, 1)}>v</button>
+                            <input
+                                type='text'
+                                placeholder='Item Text'
+                                value={list_item}
+                                onChange={props.handleListItemChange(props.index, listItemIndex)}
+                            />
 
-                        <button type='button' onClick={props.removeListItem(props.index, listItemIndex)}>-</button>
-                    </li>
-                ))}
-            </ol>
+                            <button type='button' onClick={props.removeListItem(props.index, listItemIndex)}>-</button>
+                        </li>
+                    ))}
+                </ol>
 
-            <button type='button' onClick={props.removeComponent(props.index)}>Remove Numbered List</button>
+                <button type='button' onClick={props.removeComponent(props.index)}>Remove Numbered List</button>           
+            </div>
         </>
     )
 }
--- src/components/TextboxForm.js
+++ src/components/TextboxForm.js
@@ -1,23 +1,27 @@
 import React from 'react'
+import {HiMenu} from "react-icons/hi"
 
 const TextboxForm = (props) => {
     return (
         <>
-            <label>Text Box Title</label>
-            <input 
-                type='text'
-                placeholder='Title'
-                value={props.component.title}
-                onChange={props.handleComponentTitleChange(props.index)}
-            />
+            <div className='grab-icon'><HiMenu /></div>
+            <div className='content-input-form'>
+                <label>Text Box Title</label>
+                <input 
+                    type='text'
+                    placeholder='Title'
+                    value={props.component.title}
+                    onChange={props.handleComponentTitleChange(props.index)}
+                />
 
-            <label>Text Box Content</label>
-            <input 
-                type='text'
-                value={props.component.text_content}
-                onChange={props.handleTextboxTextContentChange(props.index)}
-            />
-            <button type='button' onClick={props.removeComponent(props.index)}>Remove Text Box</button>
+                <label>Text Box Content</label>
+                <input 
+                    type='text'
+                    value={props.component.text_content}
+                    onChange={props.handleTextboxTextContentChange(props.index)}
+                />
+                <button type='button' onClick={props.removeComponent(props.index)}>Remove Text Box</button>
+            </div>
         </>      
     )
 }
