20612bb60731e664348050a31340ca9c01fd6efc
rmiverson <rmiverson99@gmail.com>
2022-12-02 11:44:32 -0800

    created a footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className='footer'>
+            <p>©Riley M. Iverson</p>
+        </footer>
+    )
+}
+
+export default Footer
