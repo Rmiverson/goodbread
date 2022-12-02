2f022261175a339e118becd544f3d171817fc7d3
rmiverson <rmiverson99@gmail.com>
2022-12-02 10:32:10 -0800

    moved add tag button to list, renamed the placeholder for tag input, and added classnames for the buttons

--- src/components/TagForm.js
+++ src/components/TagForm.js
@@ -1,22 +1,23 @@
 import React from 'react'
+import { HiOutlineX } from "react-icons/hi"
 
 const TagForm = (props) => {
     return(
         <div className='tag-form'>
             <label>Tags</label>
-            <button type='button' onClick={props.addTag}>Add Tag</button>
             <ul>
                 {props.tags.map((tag, index) => (
                     <li key={index}>
                         <input 
                             type='text'
-                            placeholder='tag name'
+                            placeholder='Tag Name'
                             value={tag.label}
                             onChange={props.handleTagChange(index)}
                         />
-                        <button type='button' onClick={props.removeTag(index)}>-</button>
+                        <button className='content-input-list-control-delete' type='button' onClick={props.removeTag(index)}><HiOutlineX /></button>
                     </li>
                 ))}
+                <button className='content-add-button' type='button' onClick={props.addTag}>Add Tag</button>
             </ul>            
         </div>
     )
