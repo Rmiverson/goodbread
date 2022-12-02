ddcf0884c3e7d264c5b3aa907ecbd42f6f784da5
rmiverson <rmiverson99@gmail.com>
2022-12-02 11:45:13 -0800

    added a footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './scss/app.scss'
 import Nav from './containers/Nav'
 import RouteBoard from './RouteBoard'
+import Footer from './components/Footer'
 
 const App = () => {  
   return (
     <div className='App'>
       <Nav />
       <RouteBoard />
+      <Footer />
     </div>
   )
 }
