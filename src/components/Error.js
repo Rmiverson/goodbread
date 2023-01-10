138f3a6d26d4bb71f23874e473a6d6ca27fc3857
rmiverson <rmiverson99@gmail.com>
2023-01-10 12:03:46 -0800

    created an error component to show errors better

---
+++ src/components/Error.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { userLogout } from '../redux/actions'
+
+const Error = (props) => {
+    const dispatch = useDispatch()
+
+    const handleLogout = () => {
+        dispatch(userLogout)
+    }
+
+    if (props.status === 401 && props.currentUser) {
+        handleLogout()
+    } else {
+        return (
+            <div className='error'>
+                <h5>{props.status}: {props.statusText}</h5>
+                <p>{props.error}</p>
+            </div>
+        )        
+    }
+}
+
+export default Error
