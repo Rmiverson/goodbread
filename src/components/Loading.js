9d2fa9bdc1cb2faa98ab9b629d50322240365a4e
rmiverson <rmiverson99@gmail.com>
2023-01-12 12:16:13 -0800

    created a loading component and added styling to it

---
+++ src/components/Loading.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../scss/loading.scss'
+
+const Loading = () => {
+    return (
+        <div className='loading'>
+            <div className="lds-facebook">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
+
+export default Loading
